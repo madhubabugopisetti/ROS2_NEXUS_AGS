c0822f84f617efa14bbe3bd6ad10a828b5f56f3b
madhu <madhubabugopisetti27@gmail.com>
2026-01-13 17:26:46 +0530

    elbow rotation fix

--- src/ags_auto/ags_auto/search_box.py
+++ src/ags_auto/ags_auto/search_box.py
@@ -11,33 +11,23 @@ import numpy as np
 class AGSSearchBox(Node):
     def __init__(self):
         super().__init__("ags_search_box")
-
         self.sub = self.create_subscription(Image, "/camera/image", self.image_cb, 10)
         self.pub = self.create_publisher(JointTrajectory, "/arm_controller/joint_trajectory", 10)
-
         self.bridge = CvBridge()
-
         self.shoulder = 0.0
         self.box_found = False
-
         self.timer = self.create_timer(0.3, self.control_loop)
-
         self.get_logger().info("AGS search box started")
 
     def image_cb(self, msg):
         img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
         lower1 = (0, 120, 70)
         upper1 = (10, 255, 255)
         lower2 = (170, 120, 70)
         upper2 = (180, 255, 255)
-
         mask = cv2.inRange(hsv, lower1, upper1) | cv2.inRange(hsv, lower2, upper2)
-
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
         self.box_found = len(contours) > 0
 
     def send_joints(self, shoulder):
@@ -51,11 +41,10 @@ class AGSSearchBox(Node):
             "left_finger_joint",
             "right_finger_joint",
         ]
-
         p = JointTrajectoryPoint()
         p.positions = [
             shoulder,
-            0.0,    # elbow
+            -0.5,    # elbow
             -1.2,    # forearm
             -1.2,   # wrist_pitch DOWN
             0.0,    # wrist_roll
